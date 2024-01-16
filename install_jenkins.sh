873103edb830d4536859405a9be259bb0a322a59
Grigory Tsuryev <tsurievga@gmail.com>
2024-01-16 17:51:29 +0200

    add

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -15,7 +15,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
 sudo apt update && \
 sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER 
 
 sudo docker network create jenkins
 
@@ -31,7 +31,7 @@ sudo docker run \
   --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  tzvitsuryev/jenkins-docker-kubectl-infra
+  tzvitsuryev/jenkins:alpine-jdk11
 
               # Run Docker-in-Docker
 sudo docker run \
